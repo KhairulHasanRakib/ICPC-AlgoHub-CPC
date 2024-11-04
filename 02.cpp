0d66e9968aab145c919771b68c9201db0bcb47df
KhairulHasanRakib <khairulhasanrakib@gmail.com>
2024-11-04 19:10:11 +0600

    practice

--- 02.cpp
+++ 02.cpp
@@ -1,23 +1,19 @@
 /*
 Imagine you have a magical garden where flowers bloom in a specific pattern.
-In the first day, a single flower blooms. 
-On the second day, two flowers bloom, and 
-On the third day, three flowers bloom. 
+In the first day, a single flower blooms.
+On the second day, two flowers bloom, and
+On the third day, three flowers bloom.
 This pattern continues. Write a program to calculate the minimum number of days needed for the garden to reach a total of at least 100 flowers.
 */
-
+#include <bits/stdc++.h>
 #include <iostream>
+using namespace std;
+int main()
+{
 
-int main() {
-    int flowers = 0; // Number of flowers in the garden
-    int days = 0; // Number of days passed
+    int n;
+    cin >> n;
+    cout << n + 1; 
 
-    while (flowers < 100) { // Loop until the number of flowers reaches 100
-        days++; // Increment the day count
-        flowers += days; // Add the number of flowers that bloom on that day
-        std::cout << "Day " << days << ": " << days << " flower(s) bloom (Total: " << flowers << ")\n";
-    }
-
-    std::cout << "It took " << days << " days to reach 100 flowers." << std::endl; // Print the number of days
-    return 0; 
+    return 0;
 }
