f08f7217458d909f8f44d26222c8f4ac8af219fb
KhairulHasanRakib <khairulhasanrakib@gmail.com>
2024-11-07 23:43:00 +0600

    string print

--- 01.cpp
+++ 01.cpp
@@ -1,13 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Function: f(x) = 3x - cos(x) - 1" << endl;
-    cout << "Enter X: ";
-    double x;
-    cin >> x;
-    cout << "f(" << x << ") = 3 * " << x << " - cos(" << x << ") - 1 = " << 3 * x - cos(x) - 1 << " is your root" << endl;
-    cout << "Your X is: " << x << endl;
-
-    return 0;
+int main()
+{
+    string s;
+    cin>>s; // input string
+    cout<<s[1]<<s[4]<<s[6]<<"\n";
+    cout<<s[1-1]<<s[4-1]<<s[6-1]<<"\n";
+    
 }
