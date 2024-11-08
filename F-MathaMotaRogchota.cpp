60ddef4149d8eee8f7338e8a6aec0053033d538b
KhairulHasanRakib <khairulhasanrakib@gmail.com>
2024-11-08 17:49:43 +0600

    mock test

---
+++ F-MathaMotaRogchota.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cout << "Ei Matha-Mota-Rogchota joddin achhe, non-profit diye kon ghaash ta katbo!" << endl;
+}
