727777c25883211343631512af3e72050606789f
KhairulHasanRakib <khairulhasanrakib@gmail.com>
2024-11-10 20:43:30 +0600

    loop

--- 01.cpp
+++ 01.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    string s;
-    cin>>s; // input string
-    cout<<s[1]<<s[4]<<s[6]<<"\n";
-    cout<<s[1-1]<<s[4-1]<<s[6-1]<<"\n";
-    
+
+    int a = 0;
+    // Remove the underscores and complete the code
+    while (a < 7)
+    {
+        cout << a << endl;
+        a = a + 1;
+    }
 }
