a51c838725acd5ef416350f05e0ef87ec589bedf
KhairulHasanRakib <khairulhasanrakib@gmail.com>
2024-11-14 20:54:10 +0600

    functions

--- 03.cpp
+++ 03.cpp
@@ -1,29 +1,19 @@
 #include <iostream>
 using namespace std;
 
-// Function to calculate C
-int calculateC(int A, int B) {
-    return A * A + 2 * A * B + B * B;
-}
-
-// Function to calculate D
-int calculateD(int A, int B) {
-    return A + B;
+void computeValue(int a, int b) {
+    // Solution
+    int c = a * a + 2 * a * b + b * b;
+    int d = a + b;
+    cout << c << "\n" << d << endl;
 }
 
 int main() {
-    // Repeat the input, calculation, and output process thrice
-    for (int i = 0; i < 3; i++) {
+    int t = 3;
+    for (int i = 0; i < t; i++) {
         int A, B;
         cin >> A >> B;
-        
-        // Use functions to calculate C and D
-        int C = calculateC(A, B);
-        int D = calculateD(A, B);
-        
-        cout << C << endl;
-        cout << D << endl;
+        computeValue(A, B);
     }
-    
     
 }
